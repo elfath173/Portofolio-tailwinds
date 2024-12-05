02b4d12f511e4dbaf6633c0e53d0ff21674cb4de
elfath173 <105049877+elfath173@users.noreply.github.com>
2024-12-05 15:09:21 +0700

    portoflio tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10b981',
+        secondary: '#64748b',
+        dark: '#020617',
+      },
+    },
+  },
+  plugins: [],
+}
+
